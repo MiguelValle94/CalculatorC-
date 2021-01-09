f8dec26f8ba98fe766bc6b0b6ab148f43b8a5bfa
miguelbachi5a <miguelbachi5a@gmail.com>
2021-01-09 11:17:21 +0100

    fix: abstarction

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -24,52 +24,57 @@ namespace Calculator
 
         private void buttonNum1_Click(object sender, EventArgs e)
         {
-            txtValue.Text = "1";
+            NumberAction("1");
         }
 
         private void buttonNum2_Click(object sender, EventArgs e)
         {
-            txtValue.Text = "2";
+            NumberAction("2");
         }
 
         private void buttonNum3_Click(object sender, EventArgs e)
         {
-            txtValue.Text = "3";
+            NumberAction("3");
         }
 
         private void buttonNum4_Click(object sender, EventArgs e)
         {
-            txtValue.Text = "4";
+            NumberAction("4");
         }
 
         private void buttonNum5_Click(object sender, EventArgs e)
         {
-            txtValue.Text = "5";
+            NumberAction("5");
         }
 
         private void buttonNum6_Click(object sender, EventArgs e)
         {
-            txtValue.Text = "6";
+            NumberAction("6");
         }
 
         private void buttonNum7_Click(object sender, EventArgs e)
         {
-            txtValue.Text = "7";
+            NumberAction("7");
         }
 
         private void buttonNum8_Click(object sender, EventArgs e)
         {
-            txtValue.Text = "8";
+            NumberAction("8");
         }
 
         private void buttonNum9_Click(object sender, EventArgs e)
         {
-            txtValue.Text = "9";
+            NumberAction("9");
         }
 
         private void buttonNum0_Click(object sender, EventArgs e)
         {
-            txtValue.Text = "0";
+            NumberAction("0");
+        }
+
+        private void NumberAction(string number)
+        {
+            txtValue.Text = number;
         }
     }
 }
